d1190664f90e7a3ceb9e5eb1933ea106673d4c0d
Levi Waldron <lwaldron.research@gmail.com>
2017-02-19 14:26:39 -0500

    Fix path to data directory

--- R/importcBioPortal.R
+++ R/importcBioPortal.R
@@ -19,7 +19,7 @@ importcBioPortal <- function(tgzfile,
                              names.field = c("Hugo_Symbol", "Entrez_Gene_Id"))
 {
     orig.dir <- getwd()
-    path <- sub(".tar.gz", "", tgzfile, fixed = TRUE)
+    path <- sub(".tar.gz", "", basename(tgzfile), fixed = TRUE)
     if(!dir.exists(path))
         untar(tgzfile, compressed = TRUE)
     fullpaths <-
